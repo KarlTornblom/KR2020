954ad851782e0d55555026e55c9a107b46dcafa4
KarlTornblom <55096906+KarlTornblom@users.noreply.github.com>
2020-06-01 16:36:19 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    session_start();
+    if($_SESSION["login"] == "?q9d$+\9YHFzb;(m2-QfH582kPH[>U-4-Z=_x.apQT3T_AjHa24#vvXf2tM:Wr8zgeCxzXV6qKzcx8B\u;!f>SZ,JS5Tp\d]#;BEY^Cd[Gb2XY<A`Czeu&E#j"){
+        header('Location: index.html');;
+    } else {
+        header('Location: login/login.html');;
+    };
+?>
